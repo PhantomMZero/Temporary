cba057b829cafa01033bfabbd49f011a4204404b
PhantomMZero <103202873+PhantomMZero@users.noreply.github.com>
2024-03-12 13:22:54 +0300

    Add files via upload

---
+++ Работа с классами и наследованием.cpp
@@ -0,0 +1,33 @@
+
+class Car {
+public:
+	double speed;
+	void gas();
+	double getFuel() {
+		return this->fuel;
+	}
+	Car() {
+		speed = 0;
+		fuel = 200;
+	}
+
+private:
+	double fuel;
+};
+
+void Car::gas() {
+	if (fuel > 0) {
+		++speed;
+		--fuel;
+	}
+}
+
+class Truck : public Car {
+public:
+	const unsigned wheels = 8;
+};
+
+class Motocycle : public Car {
+public:
+	const unsigned wheels = 2;
+};
